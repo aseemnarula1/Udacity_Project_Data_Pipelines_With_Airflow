381ca94c7e1c921cb1162886b1e9a314dee3f741
aseemnarula1 <aseem_narula07@yahoo.com>
2023-10-24 13:16:09 +0530

    Update airflow_udacity_sparkify_dag.py

--- dags/airflow_udacity_sparkify_dag.py
+++ dags/airflow_udacity_sparkify_dag.py
@@ -2,59 +2,35 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator, LoadDimensionOperator, DataQualityOperator)
-from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
-
-print("Importing Airflow Libraries Python Modules")
 
 AWS_KEY = os.environ.get('AWS_KEY')
 AWS_SECRET = os.environ.get('AWS_SECRET')
 
-# Preparing the Default Arguements for Airflow module DAG
-print("Preparing the Default Arguements for Airflow module DAG")
-
 default_args = {
-     'owner': 'udacity_airflow_project_sparkify',
-     'start_date': datetime.now(),    
-     'retries':3,
-     'retry_delay': timedelta(minutes = 5),
-     'catchup': False,
-     'email_on_retry': False,
-     'depends_on_past': False
+    'owner': 'udacity-data-engineer-nanodegree',
+    'start_date': datetime.now(),
+    'depends_on_past': False,       
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup':False
 }
-
-# Defining DAG - airflow_udacity_sparkify_dag
-print("Defining DAG - airflow_udacity_sparkify_dag")
 
 dag = DAG('airflow_udacity_sparkify_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-	      start_date = datetime.now(),	
-          schedule_interval='0 * * * *',
-          catchup = False,
-          max_active_runs=1  		
+          schedule_interval='0 * * * *'
         )
-
-# Begining with the Start Operator with the DAG execution
-print("Begining with the Start Operator with the DAG execution")
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-create_tables = PostgresOperator(
-    task_id='create_tables',
-    dag=dag,
-    postgres_conn_id="redshift",
-    sql='create_tables.sql',
-)
-
-
-# Stage to RedShift Operator for Staging Events Table
-print("Stage to RedShift Operator for Staging Events Table")
-
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id = 'Stage_events',
-    dag = dag,
+    task_id='Stage_events',
+    dag=dag,    
     redshift_conn_id = 'redshift',
     aws_credentials_id = 'aws_credentials',
     table = 'staging_events',
@@ -64,13 +40,10 @@ stage_events_to_redshift = StageToRedshiftOperator(
     region = 'us-west-2'
 )
 
-# Stage to RedShift Operator for Staging Songs Table
-print("Stage to RedShift Operator for Staging Songs Table")
-
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id = 'Stage_songs',
-    dag = dag,
-    redshift_conn_id = 'redshift',
+    task_id='Stage_songs',
+    dag=dag,
+        redshift_conn_id = 'redshift',
     aws_credentials_id = 'aws_credentials',
     table = 'staging_songs',
     s3_bucket = 'udacity-dend',
@@ -79,9 +52,6 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     region = 'us-west-2'
 )
 
-# Load Fact Operator for SongPlays Table 
-print("Load Fact Operator for SongPlays Table")
-
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag,
@@ -89,10 +59,8 @@ load_songplays_table = LoadFactOperator(
     table_name="songplays",
     sql_statement=SqlQueries.songplay_table_insert,
     append_data=False
-)
 
-# Loading User Dimension Table in RedShift
-print("Load User Dimension Table in RedShift")
+)
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
@@ -103,9 +71,6 @@ load_user_dimension_table = LoadDimensionOperator(
     append_data=False
 )
 
-# Loading Song Dimension Table in RedShift
-print("Loading Song Dimension Table in RedShift")
-
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
     dag=dag,
@@ -115,31 +80,23 @@ load_song_dimension_table = LoadDimensionOperator(
     append_data=False
 )
 
-# Loading Artist Dimension Table in RedShift
-print("Loading Artist Dimension Table in RedShift")
-
 load_artist_dimension_table = LoadDimensionOperator(
-    task_id = 'Load_time_dim_table',
-    dag = dag,
+    task_id='Load_artist_dim_table',
+    dag=dag,
     redshift_conn_id = 'redshift',
     table = 'time',
     sql = SqlQueries.time_table_insert,
     append_data=False
 )
 
-# Loading Time Dimension Table in RedShift
-print("Loading Time Dimension Table in RedShift")
-
 load_time_dimension_table = LoadDimensionOperator(
-    task_id = 'Load_time_dim_table',
-    dag = dag,
+    task_id='Load_time_dim_table',
+    dag=dag,
     redshift_conn_id = 'redshift',
     table = 'time',
     sql = SqlQueries.time_table_insert
 )
-
 # Running Data Quality Checks
-
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
     dag=dag,
@@ -153,11 +110,7 @@ run_quality_checks = DataQualityOperator(
     ]
 )
 
-# End Operator to stop the DAG Execution
-print("End Operator to stop the DAG Execution")
-
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-
 
 # DAG Task Dependency
 
